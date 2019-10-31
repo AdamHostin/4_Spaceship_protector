d868ef29ba2d33cb89ebf79ca2300705a32d44dd
Adam Hostin <hostinko@gmail.com>
2019-10-31 15:01:11 +0100

    some changes

--- Assets/Leve_load_from_splash.cs
+++ Assets/Leve_load_from_splash.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneMangement;
 public class Leve_load_from_splash : MonoBehaviour
 {
     // Start is called before the first frame update
